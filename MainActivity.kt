9c0eb47d5814829d1f1437ee82f8d8e0a31a2a79
a94284908-sudo <a94284908@gmail.com>
2025-09-10 16:14:51 +0330

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.amiko
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textView)
+        textView.text = "سلام! این اولین اپلیکیشن Kotlin شماست 🚀"
+    }
+}
